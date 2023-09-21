7c592f3121e09b41a0916ee1039fdbf8e3741fcb
Hadley Wickham <h.wickham@gmail.com>
2023-09-21 08:39:51 -0500

    Initial commit

---
+++ scrape.R
@@ -0,0 +1,21 @@
+library(rvest)
+
+url <- "http://www.westonlambert.com/available-work"
+
+html <- read_html(url)
+
+products <- html |>
+  html_element("#productList") |>
+  html_elements("a")
+
+df <- data.frame(
+  title = products |> html_element(".product-title") |> html_text(),
+  price = products |> html_element(".product-price") |> html_text() |>
+    gsub("[$,]", "", x = _) |>
+    as.numeric(),
+  sold_out = products |> html_element(".sold-out") |> html_text()
+)
+
+write.csv(df, "products.csv", row.names = FALSE)
+
+
