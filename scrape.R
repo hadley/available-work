5df2bff58f1021609e9df9725602a5993cd01d87
Hadley Wickham <h.wickham@gmail.com>
2024-06-12 15:56:09 -0500

    Improvements from live coding

--- scrape.R
+++ scrape.R
@@ -10,6 +10,10 @@ products <- html |>
   html_element("#productList") |>
   html_elements("a")
 
+if (length(products) == 0) {
+  stop("No products found! Did the website change?")
+}
+
 cur <- data.frame(
   title = products |> html_element(".product-title") |> html_text(),
   price = products |> html_element(".product-price") |> html_text() |>
@@ -18,12 +22,22 @@ cur <- data.frame(
   sold_out = products |> html_element(".sold-out") |> html_text(),
   link = html_attr(products, "href")
 )
+cur
+
+if (cur$price < 0) {
+  stop("Found negative price! Something went wrong")
+}
 
 old <- read.csv("products.csv")
 write.csv(cur, "products.csv", row.names = FALSE)
 
+# Find all products that aren't sold out, and I didn't see last time
 new <- subset(cur, is.na(sold_out) & !link %in% old$link)
 if (nrow(new) > 0) {
   msg <- paste0(nrow(new), " products available at ", url)
-  ntfy::ntfy_send(msg, topic = "BjFR7fMVkYMSsFrS")
+  request("https://ntfy.sh/") |>
+    req_url_path("BjFR7fMVkYMSsFrS") |>
+    req_body_raw(msg) |>
+    req_headers(Title = "Update at Weston Lambert", Click = "https://google.com") |>
+    req_perform()
 }
